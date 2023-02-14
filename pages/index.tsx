563bff368b431d717df98b479f8b2f4ff689149f
La <35848139+la0311@users.noreply.github.com>
2023-02-15 00:03:06 +0700

    Implement index.tsx - Main video page Use getServerSideProps and axios to get data from api/post Copy code from hello.ts to post/index.ts and delete hello.ts Implement post/index.ts: - Query allPostsQuery - Edit client (in utils) and fetch:   + Get projectId in Sanity   + Get token in Sanity and add to .env.development   + Add localhost port to CORS Origin in Sanity - Add res.status 200 + jason data Create interface props to define videos type - Create types.d.ts - Add Video interface to videos Create and add VideoCard and NoResults component to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import type { NextPage } from "next";
+import axios from "axios";
+import { Video } from "./../types.d";
+import VideoCard from "./../components/VideoCard";
+import NoResults from "./../components/NoResults";
+
+interface IProps {
+  videos: Video[];
 }
+
+export default function Home({ videos }: IProps) {
+  console.log(videos);
+  return (
+    <div
+      className="flex flex-col gap-10 videos
+   h-full"
+    >
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"No Videos"} />
+      )}
+    </div>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+  return {
+    props: { videos: data },
+  };
+};
